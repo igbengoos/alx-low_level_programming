2086b870dd96cd198f9b23a65e18f2080a3facd1
Oluwagbenga Adeleye <Gbenga Adeleye>
2022-07-05 11:36:07 -0700

    Script that check value and estimate

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: int type number
+ * Return: absolute value of @n
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (r * -1);
+	}
+	else
+	{
+		return (r);
+	}
+}
